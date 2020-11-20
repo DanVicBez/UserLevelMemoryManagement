de2a3cef5fad7a4498ae8d86c321bb5211065920
Daniel V. Bezden <dvb27@man.cs.rutgers.edu>
2020-11-19 20:39:51 -0500

    Initial commit

---
+++ my_vm.c
@@ -0,0 +1,169 @@
+#include "my_vm.h"
+
+/*
+Function responsible for allocating and setting your physical memory
+*/
+void SetPhysicalMem() {
+
+    //Allocate physical memory using mmap or malloc; this is the total size of
+    //your memory you are simulating
+
+
+    //HINT: Also calculate the number of physical and virtual pages and allocate
+    //virtual and physical bitmaps and initialize them
+
+}
+
+
+/*
+ * Part 2: Add a virtual to physical page translation to the TLB.
+ * Feel free to extend the function arguments or return type.
+ */
+int
+add_TLB(void *va, void *pa)
+{
+
+    /*Part 2 HINT: Add a virtual to physical page translation to the TLB */
+
+    return -1;
+}
+
+
+/*
+ * Part 2: Check TLB for a valid translation.
+ * Returns the physical page address.
+ * Feel free to extend this function and change the return type.
+ */
+pte_t *
+check_TLB(void *va) {
+
+    /* Part 2: TLB lookup code here */
+
+}
+
+
+/*
+ * Part 2: Print TLB miss rate.
+ * Feel free to extend the function arguments or return type.
+ */
+void
+print_TLB_missrate()
+{
+    double miss_rate = 0;
+
+    /*Part 2 Code here to calculate and print the TLB miss rate*/
+
+
+
+
+    fprintf(stderr, "TLB miss rate %lf \n", miss_rate);
+}
+
+
+/*
+The function takes a virtual address and page directories starting address and
+performs translation to return the physical address
+*/
+pte_t * Translate(pde_t *pgdir, void *va) {
+    //HINT: Get the Page directory index (1st level) Then get the
+    //2nd-level-page table index using the virtual address.  Using the page
+    //directory index and page table index get the physical address
+
+
+    //If translation not successfull
+    return NULL;
+}
+
+
+/*
+The function takes a page directory address, virtual address, physical address
+as an argument, and sets a page table entry. This function will walk the page
+directory to see if there is an existing mapping for a virtual address. If the
+virtual address is not present, then a new entry will be added
+*/
+int
+PageMap(pde_t *pgdir, void *va, void *pa)
+{
+
+    /*HINT: Similar to Translate(), find the page directory (1st level)
+    and page table (2nd-level) indices. If no mapping exists, set the
+    virtual to physical mapping */
+
+    return -1;
+}
+
+
+/*Function that gets the next available page
+*/
+void *get_next_avail(int num_pages) {
+
+    //Use virtual address bitmap to find the next free page
+}
+
+
+/* Function responsible for allocating pages
+and used by the benchmark
+*/
+void *myalloc(unsigned int num_bytes) {
+
+    //HINT: If the physical memory is not yet initialized, then allocate and initialize.
+
+   /* HINT: If the page directory is not initialized, then initialize the
+   page directory. Next, using get_next_avail(), check if there are free pages. If
+   free pages are available, set the bitmaps and map a new page. Note, you will
+   have to mark which physical pages are used. */
+
+    return NULL;
+}
+
+/* Responsible for releasing one or more memory pages using virtual address (va)
+*/
+void myfree(void *va, int size) {
+
+    //Free the page table entries starting from this virtual address (va)
+    // Also mark the pages free in the bitmap
+    //Only free if the memory from "va" to va+size is valid
+}
+
+
+/* The function copies data pointed by "val" to physical
+ * memory pages using virtual address (va)
+*/
+void PutVal(void *va, void *val, int size) {
+
+    /* HINT: Using the virtual address and Translate(), find the physical page. Copy
+       the contents of "val" to a physical page. NOTE: The "size" value can be larger
+       than one page. Therefore, you may have to find multiple pages using Translate()
+       function.*/
+
+}
+
+
+/*Given a virtual address, this function copies the contents of the page to val*/
+void GetVal(void *va, void *val, int size) {
+
+    /* HINT: put the values pointed to by "va" inside the physical memory at given
+    "val" address. Assume you can access "val" directly by derefencing them.
+    If you are implementing TLB,  always check first the presence of translation
+    in TLB before proceeding forward */
+
+
+}
+
+
+
+/*
+This function receives two matrices mat1 and mat2 as an argument with size
+argument representing the number of rows and columns. After performing matrix
+multiplication, copy the result to answer.
+*/
+void MatMult(void *mat1, void *mat2, int size, void *answer) {
+
+    /* Hint: You will index as [i * size + j] where  "i, j" are the indices of the
+    matrix accessed. Similar to the code in test.c, you will use GetVal() to
+    load each element and perform multiplication. Take a look at test.c! In addition to
+    getting the values from two matrices, you will perform multiplication and
+    store the result to the "answer array"*/
+
+
+}
